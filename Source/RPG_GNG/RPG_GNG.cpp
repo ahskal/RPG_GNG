2a88de1e494d3f4253a14ccca70b2b1035d518ea
ahskal <kkj380377@gmail.com>
2024-10-20 09:20:28 +0900

    Unreal file Commit

---
+++ Source/RPG_GNG/RPG_GNG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RPG_GNG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPG_GNG, "RPG_GNG" );
